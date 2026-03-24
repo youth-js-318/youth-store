b08fc046db23690839f5b06f881ec3ed9985683f
HuygnesPython <huygneszindocsgo@gmail.com>
2026-03-24 19:15:35 -0300

    Request para produtos

--- src/pages/HomePage/index.tsx
+++ src/pages/HomePage/index.tsx
@@ -1,5 +1,20 @@
+import { useEffect, useState } from "react"
 
 const HomePage = () => {
+    const [products, setProducts] = useState([])
+
+    useEffect(
+        // consequência
+        () => {
+            fetch('https://fakestoreapi.com/products')
+                .then(res => res.json())
+                .then(json => console.log(json))
+        },
+
+        // causa
+        []
+    )
+
     return (
         <h1>Home Page</h1>
     )
